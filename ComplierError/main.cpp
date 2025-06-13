22a7ef5888487d98ca0bd9519e2fd51db58f1f80
Anger Dooshima Lois <danielladoosh@gmail.com>
2025-06-13 12:10:22 +0100

    Add files via upload

---
+++ ComplierError/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+//extern int x;
+int main() {
+	//missing terminating character
+	//std::cout << "Hello world <<std::endl;
+
+	std::cout << "Hello world" << std::endl;
+
+	//undefine miles_driven
+	/*int miles_driven;
+	std::cout << miles_driven;*/
+
+	//return; //error return no tatement with no value
+	//complier warnings dont ignore them
+
+	//linked error undefine error to x because it wasnt defined in the main 
+	//std::cout << x;
+
+	//runtime errors
+	//errors that occur when the program is executing
+
+
+	//Logic Error
+	//errors or bugs in your code that cause your program to run incorrectly
+	//logic error are mistake programmer make
+	
+	/*if (age > 18)
+		std::cout << "Yes, you can vote! ";*/
+
+
+	return 0;
+
+}
